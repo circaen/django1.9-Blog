f5c8fb48caf2b86d5eb04da85799d5e0315d662b
circaen <circaen00@gmail.com>
2016-05-27 00:06:04 -0500

    Add files via upload

---
+++ p5/blog/views.py
@@ -0,0 +1,75 @@
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import PostForm
+from .models import Post
+
+
+def blog_create(request):
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "<a href='#'>Item</a> Created", extra_tags='html_safe')
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, "blog/form.html", context)
+
+
+def blog_detail(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+    }
+    return render(request, "blog/detail.html", context)
+
+
+def blog_list(request):
+    queryset_list = Post.objects.all() #.order_by("-timestamp")
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
+    context = {
+        "object_list": queryset,
+        "title": "List",
+        "page_request_var": page_request_var,
+    }
+    return render(request, "blog/list.html", context)
+
+
+
+def blog_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "blog/form.html", context)
+
+
+def blog_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully deleted")
+    return redirect("blog:list")
